523af2f534b236893a9c323ca565d09e5fea0330
thestungyer <47674413+thestungyer@users.noreply.github.com>
2022-12-26 16:40:25 -0700

    Hello World!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {ThemeProvider} from '@primer/react'
+import {BaseStyles, Box, Heading} from '@primer/react'
+
+
+const theme = {
+  colors: {
+    canvas: {
+      default: '#fff'
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Maybe?
-        </a>
-      </header>
-    </div>
+    <BaseStyles>
+    <Box m={4}>
+      <Heading sx={{mb: 2}}>Hello, world!</Heading>
+    </Box>
+  </BaseStyles>
   );
 }
 
