06a60056e8561bbc54111a62c614a88565e1113b
Mohammed Alzafarani <mohammefm@gmail.com>
2023-08-25 07:17:12 -0600

    updates

---
+++ personalwebsite/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi my name is Mohammed Alzafarani. I am a Software Engineer</h1>
+      <h1>Hello, ksk</h1>
+      
+      
+
+    </div>
+  );
+}
+
+export default App;
